2992569d09b64d09f90659880779801c931b0c26
Jason Ozias <jason.g.ozias@gmail.com>
2017-08-04 08:48:05 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+// Copyright (c) 2017 tyr developers
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. All files in the project carrying such notice may not be copied,
+// modified, or distributed except according to those terms.
+
+//! `tyr` 0.1.0
+#![deny(missing_docs)]
+#[macro_use]
+extern crate error_chain;
+extern crate clap;
+
+mod error;
+mod run;
+
+use std::io::{self, Write};
+use std::process;
+
+/// CLI Entry Point
+fn main() {
+    match run::run() {
+        Ok(i) => process::exit(i),
+        Err(e) => {
+            writeln!(io::stderr(), "{}", e).expect("Unable to write to stderr!");
+            process::exit(1)
+        }
+    }
+}
